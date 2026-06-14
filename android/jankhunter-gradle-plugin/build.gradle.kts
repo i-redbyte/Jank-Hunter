4c5186589a8bcf3aa6a69042ebd1e17a9fd1294b
red_byte <developer.sokolov@gmail.com>
2026-06-14 13:45:28 +0300

    Publish Gradle plugin for Java 17

--- android/jankhunter-gradle-plugin/build.gradle.kts
+++ android/jankhunter-gradle-plugin/build.gradle.kts
@@ -1,7 +1,25 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     `java-gradle-plugin`
     id("org.jetbrains.kotlin.jvm")
     id("maven-publish")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.release.set(17)
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_17)
+    }
 }
 
 gradlePlugin {
